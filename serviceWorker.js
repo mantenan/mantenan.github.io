4ad66428e5885ab1b1251e872d5080b5a4e7ec22
mantenan <70057977+mantenan@users.noreply.github.com>
2020-09-17 17:13:54 +0700

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,37 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+	"/",
+	"/index.html",
+	"/assets/css/main.min.css",
+	"/assets/js/main.min.js",
+	"/assets/icon/favicon.ico",
+	"/assets/icon/icon-16x16.png",
+	"/assets/icon/icon-32x32.png",
+	"/assets/icon/icon-72x72.png",
+	"/assets/icon/icon-128x128.png",
+	"/assets/icon/icon-192x192.png",
+	"/assets/icon/icon-256x256.png",
+	"/assets/icon/icon-512x512.png",
+	"/assets/img/bg-1.jpg",
+	"/assets/img/bg-2.jpg",
+	"/assets/img/image-default.svg",
+	"/assets/img/logo-mantenan.png",
+	"/assets/img/main.png",
+	"/assets/img/open-graph-protocol.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+	installEvent.waitUntil(
+		caches.open(staticDevCoffee).then(cache => {
+			cache.addAll(assets)
+		})
+		)
+})
+
+self.addEventListener("fetch", fetchEvent => {
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then(res => {
+			return res || fetch(fetchEvent.request)
+		})
+	)
+})
