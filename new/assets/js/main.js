defc3e7f037ba85bdcf007f65335d77fb212d791
mantenan <70057977+mantenan@users.noreply.github.com>
2021-05-12 01:02:34 +0700

    Add files via upload

--- new/assets/js/main.js
+++ new/assets/js/main.js
@@ -126,8 +126,9 @@
 		var target = this.hash
 		$target = $(target)
 		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top - 100
+			'scrollTop': $target.offset().top - 72
 		},500)
+		if ( $('#menu').hasClass('show') ) $('.navbar-toggler').click()
 	})
 
 // Order
